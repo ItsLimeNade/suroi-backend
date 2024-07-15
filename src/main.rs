e9c097e7fa8c4203364be80d52a67c2af2b60617
LimeNade <alexandre.zarkua@gmail.com>
2024-07-16 01:21:34 +0200

    Bumped Vec2D to f64 + new methods

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,12 @@
+use utils::vectors::Vec2D;
+
+mod utils;
+
 fn main() {
+
+    let vec: Vec2D = Vec2D::normalize(&Vec2D::new(5.0, 10.0), &None);
+    let vec2: Vec2D = Vec2D::normalize(&Vec2D::new(5.0, 10.0), &Some(Vec2D::new(1.0, 0.0)));
+    let vec3: Vec2D = Vec2D::normalize(&Vec2D::new(5.0, 10.0), &Some(Vec2D::new(5.0, 3.0)));
+
+    println!("First:{vec:?}, Second:{vec2:?}, Third:{vec3:?}")
 }
--- src/utils/vectors.rs
+++ src/utils/vectors.rs
@@ -1,9 +1,9 @@
-use std::ops::{Add, Mul, Sub, Neg};
+use std::ops::{Add, Mul, Sub, Neg, MulAssign};
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Vec2D {
-    x: f32,
-    y: f32
+    x: f64,
+    y: f64
 }
 
 impl Add for Vec2D {
@@ -29,10 +29,17 @@ impl Sub for Vec2D {
 }
 
 impl Mul for Vec2D {
-    type Output = f32;
+    type Output = f64;
 
-    fn mul(self, other: Vec2D) -> f32 {
+    fn mul(self, other: Vec2D) -> f64 {
         self.x * other.x + self.y * other.y
+    }
+}
+
+impl MulAssign<f64> for Vec2D {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
     }
 }
 
@@ -48,12 +55,12 @@ impl Neg for Vec2D {
 }
 
 impl Vec2D {
-    pub fn new(x: f32, y: f32) -> Self {
+    pub fn new(x: f64, y: f64) -> Self {
         Vec2D {
             x,
             y
         }
-    }   
+    }
 
     pub fn add(vector1: &Vec2D, vector2: &Vec2D) -> Self {
         Vec2D {
@@ -69,7 +76,7 @@ impl Vec2D {
         }
     }
 
-    pub fn scale(&self, amplifier: f32) -> Self {
+    pub fn scale(&self, amplifier: f64) -> Self {
         Vec2D {
             x: &self.x * amplifier,
             y: &self.y * amplifier
@@ -83,29 +90,29 @@ impl Vec2D {
         }
     }
 
-    pub fn rotate(vector: &Vec2D, angle: f32) -> Self {
-        let cos: f32 = f32::cos(angle);
-        let sin: f32 = f32::sin(angle);
+    pub fn rotate(vector: &Vec2D, angle: f64) -> Self {
+        let cos: f64 = f64::cos(angle);
+        let sin: f64 = f64::sin(angle);
         Vec2D {
             x: vector.x * cos - vector.y * sin,
             y: vector.x * sin + vector.y * cos
         }
     }
 
-    pub fn squared_length(vector: &Vec2D) -> f32 {
+    pub fn squared_length(vector: &Vec2D) -> f64 {
         vector.x * vector.x + vector.y * vector.y
     }
 
-    pub fn length(vector: &Vec2D) -> f32 {
-        f32::sqrt(Vec2D::squared_length(vector))
+    pub fn length(vector: &Vec2D) -> f64 {
+        f64::sqrt(Vec2D::squared_length(vector))
     }
 
-    pub fn dot_product(vec1: &Vec2D, vec2: &Vec2D) -> f32 {
+    pub fn dot_product(vec1: &Vec2D, vec2: &Vec2D) -> f64 {
         vec1.x * vec2.x + vec1.y * vec2.y
     }
 
-    pub fn direction(vector: &Vec2D) -> f32 {
-        f32::atan2(vector.y, vector.x)
+    pub fn direction(vector: &Vec2D) -> f64 {
+        f64::atan2(vector.y, vector.x)
     }
 
     pub fn invert(vector: &Vec2D) -> Vec2D {
@@ -115,7 +122,51 @@ impl Vec2D {
         }
     }
 
-    pub fn ang_vec(vec1: &Vec2D, vec2: &Vec2D)  {
-        print!("lol")
+    pub fn angle(vec1: &Vec2D, vec2: &Vec2D) -> f64  {
+        f64::acos((vec1.x * vec2.x + vec1.y * vec2.y) / f64::sqrt(Vec2D::length(vec1) * Vec2D::length(vec2)))
+    }
+
+    pub fn lerp(start: &Vec2D, end: &Vec2D, interp_factor: &f64) -> Self {
+        Vec2D::scale(start, 1.0 - interp_factor) + Vec2D::scale(end, *interp_factor)
+    }
+
+    pub fn project(vec1: &Vec2D, vec2: &Vec2D) -> Self {
+        Vec2D::scale(vec2, Vec2D::dot_product(vec1, vec2) / Vec2D::squared_length(vec2))
+    }
+
+    pub fn normalize(vector: &Vec2D, fallback: &Option<Vec2D>) -> Self {
+        let fallback: Vec2D = match fallback {
+            Some(thing) => thing.clone(),
+            None => Vec2D::new(1.0, 0.0),
+        };
+        let len = Vec2D::length(vector);
+
+        if len > f64::from(0.000001) {
+            Vec2D {
+                x: vector.x / len,
+                y: vector.y / len
+            }
+        } else {
+            fallback
+        }
+    }
+
+    pub fn equals(vec1: &Vec2D, vec2: &Vec2D, epsilon: &Option<f64>) -> bool {
+        let epsilon: f64 = match epsilon {
+            Some(eps) => eps.clone(),
+            None => f64::from(0.001),
+        };
+        f64::abs(vec1.x - vec2.x) <= epsilon && f64::abs(vec1.y - vec2.y) <=epsilon
+    }
+
+    pub fn from_polar(angle: &f64, magnitude: &Option<f64>) -> Self {
+        let magnitude: f64 = match magnitude {
+            Some(mag) => mag.clone(),
+            None => f64::from(1.0),
+        };
+        Vec2D {
+            x: f64::cos(*angle) * magnitude,
+            y: f64::sin(*angle) * magnitude
+        }
     }
 }
