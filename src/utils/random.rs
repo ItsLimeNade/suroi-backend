0e7e68c5656d6c3b6af59bcebb9a9b854385cb44
LimeNade <alexandre.zarkua@gmail.com>
2024-07-15 02:08:07 +0200

    10/21 Vec2D methods

--- src/utils/random.rs
+++ src/utils/random.rs
@@ -1,14 +1,14 @@
 use rand::prelude::*;
 
-pub fn weighted_random<'a, T>(items: &'a Vec<T>, weights: &'a Vec<f64>) -> &'a T {
+pub fn weighted_random<'a, T>(items: &'a [T], weights: &'a Vec<f64>) -> &'a T {
     let mut i: usize = 0;
     let mut pick: f64 = rand::thread_rng().gen::<f64>() * weights.iter().sum::<f64>();
     
     loop {
         pick -= weights[i];
         i += 1;
-        if pick <= 0.0 {break;}
+        if pick <= 0.0 {
+            return &items[i-1];
+        }
     }
-
-    &items[i-1]
 }
--- src/utils/vectors.rs
+++ src/utils/vectors.rs
@@ -26,13 +26,38 @@ impl Vec2D {
         }
     }
 
-    pub fn mult(&self, amplifier: f32) -> Vec2D {
+    pub fn scale(&self, amplifier: f32) -> Self {
         Vec2D {
             x: &self.x * amplifier,
             y: &self.y * amplifier
         }
     }
+
+    pub fn clone(&self) -> Self {
+        Vec2D {
+            x: self.x,
+            y: self.y
+        }
+    }
+
+    pub fn rotate(vector: &Vec2D, angle: f32) -> Self {
+        let cos: f32 = f32::cos(angle);
+        let sin: f32 = f32::sin(angle);
+        Vec2D {
+            x: vector.x * cos - vector.y * sin,
+            y: vector.x * sin + vector.y * cos
+        }
+    }
+
+    pub fn squared_length(vector: &Vec2D) -> f32 {
+        vector.x * vector.x + vector.y * vector.y
+    }
+
+    pub fn length(vector: &Vec2D) -> f32 {
+        f32::sqrt(vector.squared_length(vector))
+    }
 }
+
 
 
 
