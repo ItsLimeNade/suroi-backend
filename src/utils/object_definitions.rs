eb72f0cc2eb717321d6e582cf2fc59f48531ac56
480L <167631801+480L@users.noreply.github.com>
2024-07-28 16:44:52 +0300

    Create object_definitions.rs

---
+++ src/utils/object_definitions.rs
@@ -0,0 +1,9 @@
+pub enum CONTAINER_TINTS {
+    White = 0xc0c0c0,
+    Red = 0xa32900,
+    Green = 0x00a30e,
+    Blue = 0x005fa3,
+    Yellow = 0xcccc00,
+}
+
+// Please port this file. Remove this comment when this file has been ported.
